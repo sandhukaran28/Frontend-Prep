10a790225d47d4f0e4dfe45c515b03f841124531
Karan Sandhu <sandhukaran2821@gmail.com>
2025-08-16 11:34:14 +1000

    New Question Added

---
+++ Prototype_Inheritance.js
@@ -0,0 +1,26 @@
+function Person(name){
+    this.name= name;
+}
+
+Person.prototype.hello = function(){
+    return `Hi ${this.name}`;
+}
+
+function Dev(name,title){
+    this.name = name;
+    this.title = title;
+}
+
+Dev.prototype = Object.create(Person.prototype);
+
+Dev.prototype.constructor = Dev;
+
+Dev.prototype.getTitle = function() {
+ return this.title;
+ }
+
+
+
+ const obj = new Dev("Alice", "Software Engineer");
+ console.log(obj.hello()); // Output: Hello Alice
+ console.log(obj.getTitle()); // Output: Software Enginee
